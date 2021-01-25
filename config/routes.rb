87babb81bae9b8d6e5883f043a7f8acd8c0be226
chinoxchen <chienfu.udp@gmail.com>
2021-01-25 00:08:35 -0300

    fix post and get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Spree::Core::Engine.routes.draw do
   post '/pagofacil', to: "pagofacil#pay", as: :pagofacil
-  get '/pagofacil/success/:payment', to: "pagofacil#success", as: :pagofacil_success
-  post '/pagofacil/success/:payment', to: "pagofacil#success", as: :pagofacil_success
+  match '/pagofacil/success/:payment', to: "pagofacil#success", as: :pagofacil_success, via: [:get, :post]
   get '/pagofacil/cancel/:payment', to: "pagofacil#cancel", as: :pagofacil_cancel
   post '/pagofacil/notify', to: 'pagofacil#notify'
 end
